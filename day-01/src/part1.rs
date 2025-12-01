3afa9241084bed2c14f8a0e008c79e94d08ee0fe
David Axelsson <davidaxelsson5@gmail.com>
2025-12-01 18:01:01 +0100

    Day 01 complete

--- day-01/src/part1.rs
+++ day-01/src/part1.rs
@@ -1,6 +1,37 @@
 #[must_use]
-pub fn task(_input: &str) -> Option<String> {
-    None
+pub fn task(input: &str) -> Option<String> {
+    let (_, res) = input
+        .lines()
+        .filter_map(|line| {
+            let dir = line.get(0..1)?;
+            let value = line.get(1..)?.parse::<u32>().ok()?;
+            Some((dir, value))
+        })
+        .fold((50u32, 0), |(angle, count), (dir, val)| {
+            let angle = match dir {
+                "L" => rotate_left(angle, val),
+                "R" => rotate_right(angle, val),
+                _ => unreachable!("Unexpected direction: {dir}"),
+            };
+            (angle, count + u32::from(angle == 0))
+        });
+
+    Some(res.to_string())
+}
+
+fn rotate_left(angle: u32, change: u32) -> u32 {
+    let change = change % 100;
+    if angle < change {
+        100 - (change - angle)
+    } else {
+        angle - change
+    }
+}
+
+fn rotate_right(angle: u32, change: u32) -> u32 {
+    let change = change % 100;
+    let sum = angle + change;
+    if sum >= 100 { sum - 100 } else { sum }
 }
 
 #[cfg(test)]
@@ -8,7 +39,6 @@ mod tests {
     use super::*;
 
     #[test]
-    #[ignore = "Not implemented"]
     fn test_task() {
         let input = include_str!("../example.txt");
         let result = task(input);
--- day-01/src/part2.rs
+++ day-01/src/part2.rs
@@ -1,6 +1,38 @@
 #[must_use]
-pub fn task(_input: &str) -> Option<String> {
-    None
+pub fn task(input: &str) -> Option<String> {
+    let (_, res) = input
+        .lines()
+        .filter_map(|line| {
+            let dir = line.get(0..1)?;
+            let value = line.get(1..)?.parse::<u32>().ok()?;
+            Some((dir, value))
+        })
+        .fold((50, 0), |(angle, count), (dir, val)| {
+            let (angle, zeros) = match dir {
+                "L" => rotate_left(angle, val),
+                "R" => rotate_right(angle, val),
+                _ => unreachable!("Unexpected direction: {dir}"),
+            };
+            (angle, count + zeros)
+        });
+
+    Some(res.to_string())
+}
+
+fn rotate_left(angle: u32, change: u32) -> (u32, u32) {
+    let wraps = change / 100;
+    let rem = change % 100;
+    let new_angle = (angle + 100 - rem) % 100;
+    let crossings = u32::from(angle != 0 && rem >= angle) + wraps;
+    (new_angle, crossings)
+}
+
+fn rotate_right(angle: u32, change: u32) -> (u32, u32) {
+    let wraps = change / 100;
+    let rem = change % 100;
+    let new_angle = (angle + rem) % 100;
+    let crossings = u32::from(angle + rem >= 100) + wraps;
+    (new_angle, crossings)
 }
 
 #[cfg(test)]
@@ -8,12 +40,10 @@ mod tests {
     use super::*;
 
     #[test]
-    // TODO: Implement test, and remove ignore.
-    #[ignore = "Not implemented"]
     fn test_task() {
         let input = include_str!("../example.txt");
         let result = task(input);
         assert!(result.is_some());
-        assert_eq!(result.unwrap(), "");
+        assert_eq!(result.unwrap(), "6");
     }
 }

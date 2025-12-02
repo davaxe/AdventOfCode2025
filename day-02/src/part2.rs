e10534124c104d04278420b5e83683a05415c8c6
David Axelsson <david.axelsson@liu.se>
2025-12-02 11:05:51 +0100

    Day 02 complete

--- day-02/src/part2.rs
+++ day-02/src/part2.rs
@@ -1,6 +1,40 @@
 #[must_use]
-pub fn task(_input: &str) -> Option<String> {
-    None
+pub fn task(input: &str) -> Option<String> {
+    let res: u64 = input
+        .split(',')
+        // Filter map to avoid explicit unwraps (assume well-formed input)
+        .filter_map(|range| {
+            let (start, end) = range.split_once('-')?;
+            let (start, end) = (start.trim().parse().ok()?, end.trim().parse().ok()?);
+            Some(invalid_ids(start, end))
+        })
+        .sum();
+    Some(res.to_string())
+}
+
+fn invalid_ids(start: u64, end: u64) -> u64 {
+    let mut sum = 0;
+    for id in start..=end {
+        let s = id.to_string();
+        if is_repeating(&s) {
+            sum += id;
+        }
+    }
+    sum
+}
+
+fn is_repeating(value: &str) -> bool {
+    for i in 1..=value.len() / 2 {
+        if !value.len().is_multiple_of(i) {
+            continue;
+        }
+
+        let pattern = &value[..i];
+        if pattern.repeat(value.len() / i) == value {
+            return true;
+        }
+    }
+    false
 }
 
 #[cfg(test)]
@@ -8,12 +42,10 @@ mod tests {
     use super::*;
 
     #[test]
-    // TODO: Implement test, and remove ignore.
-    #[ignore = "Not implemented"]
     fn test_task() {
         let input = include_str!("../example.txt");
         let result = task(input);
         assert!(result.is_some());
-        assert_eq!(result.unwrap(), "");
+        assert_eq!(result.unwrap(), "4174379265");
     }
 }

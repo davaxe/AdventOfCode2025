a41050f7ffcd58ae9868d5295e0b8efa3ad2442c
David Axelsson <davidaxelsson5@gmail.com>
2025-12-03 16:44:39 +0100

    Day 03 complete

--- day-03/src/part1.rs
+++ day-03/src/part1.rs
@@ -1,6 +1,31 @@
 #[must_use]
-pub fn task(_input: &str) -> Option<String> {
-    None
+pub fn task(input: &str) -> Option<String> {
+    let res: u32 = input
+        .lines()
+        .map(|line| {
+            max_jolts(
+                line.chars()
+                    .map(|c| u32::from(c) - '0' as u32)
+                    .collect::<Vec<u32>>()
+                    .as_slice(),
+            )
+        })
+        .sum();
+
+    Some(res.to_string())
+}
+
+fn max_jolts(bank: &[u32]) -> u32 {
+    let mut max = 0;
+    for (left_i, left) in bank.iter().enumerate() {
+        for right in bank.iter().skip(left_i + 1) {
+            let value = left * 10 + right;
+            if value > max {
+                max = value;
+            }
+        }
+    }
+    max
 }
 
 #[cfg(test)]
@@ -8,12 +33,10 @@ mod tests {
     use super::*;
 
     #[test]
-    // TODO: Implement test, and remove ignore.
-    #[ignore = "Not implemented"]
     fn test_task() {
         let input = include_str!("../example.txt");
         let result = task(input);
         assert!(result.is_some());
-        assert_eq!(result.unwrap(), "");
+        assert_eq!(result.unwrap(), "357");
     }
 }
--- day-03/src/part2.rs
+++ day-03/src/part2.rs
@@ -1,5 +1,40 @@
+use std::cmp::Reverse;
+use std::collections::BinaryHeap;
 #[must_use]
-pub fn task(_input: &str) -> Option<String> {
+pub fn task(input: &str) -> Option<String> {
+    let res: u64 = input
+        .lines()
+        .filter_map(|line| {
+            max_jolts(
+                line.chars()
+                    .map(|c| u64::from(c) - '0' as u64)
+                    .collect::<Vec<u64>>()
+                    .as_slice(),
+                12,
+            )
+        })
+        .sum();
+
+    Some(res.to_string())
+}
+
+/// Finds the maximum joltage value that can be formed by selecting `n` digits
+/// from the given bank of digits, maintaining their relative order.
+fn max_jolts(bank: &[u64], n: u32) -> Option<u64> {
+    let mut queue = BinaryHeap::new();
+
+    // Prioritize the largest values, then the smallest index
+    queue.push((0, Reverse(0), n));
+    while let Some((joltage, Reverse(pos), remaining_digits)) = queue.pop() {
+        if remaining_digits == 0 {
+            return Some(joltage);
+        }
+        for (pos, next) in bank.iter().enumerate().skip(pos) {
+            let next_state = (joltage * 10 + next, Reverse(pos + 1), remaining_digits - 1);
+            queue.push(next_state);
+        }
+    }
+
     None
 }
 
@@ -8,12 +43,10 @@ mod tests {
     use super::*;
 
     #[test]
-    // TODO: Implement test, and remove ignore.
-    #[ignore = "Not implemented"]
     fn test_task() {
         let input = include_str!("../example.txt");
         let result = task(input);
         assert!(result.is_some());
-        assert_eq!(result.unwrap(), "");
+        assert_eq!(result.unwrap(), "3121910778619");
     }
 }

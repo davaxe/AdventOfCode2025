92e7d7f145ee7ac3337ceb81f7615984c08ddf0a
David Axelsson <davidaxelsson5@gmail.com>
2025-12-04 17:42:24 +0100

    Day 04 complete

--- day-04/src/part2.rs
+++ day-04/src/part2.rs
@@ -1,6 +1,45 @@
+use crate::part1;
+use std::collections::HashMap;
+
+#[allow(clippy::cast_possible_truncation, clippy::cast_possible_wrap)]
 #[must_use]
-pub fn task(_input: &str) -> Option<String> {
-    None
+pub fn task(input: &str) -> Option<String> {
+    let mut map = input
+        .lines()
+        .enumerate()
+        .flat_map(|(y, line)| {
+            line.char_indices().map(move |(x, c)| match c {
+                '@' => ((x as i32, y as i32), part1::Cell::NonEmpty),
+                '.' => ((x as i32, y as i32), part1::Cell::Empty),
+                _ => unreachable!(),
+            })
+        })
+        .collect::<HashMap<_, _>>();
+
+    let mut res = 0;
+    loop {
+        let changed = step(&mut map);
+        if changed == 0 {
+            break;
+        }
+        res += changed;
+    }
+
+    Some(res.to_string())
+}
+
+fn step(map: &mut HashMap<(i32, i32), part1::Cell>) -> usize {
+    let updates: Vec<(i32, i32)> = map
+        .iter()
+        .filter_map(|(pos, c)| (c == &part1::Cell::NonEmpty).then_some(*pos))
+        .filter(|(x, y)| part1::count_adjacent_non_empty(map, *x, *y) >= 5)
+        .collect();
+
+    for pos in &updates {
+        map.insert(*pos, part1::Cell::Empty);
+    }
+
+    updates.len()
 }
 
 #[cfg(test)]
@@ -8,12 +47,10 @@ mod tests {
     use super::*;
 
     #[test]
-    // TODO: Implement test, and remove ignore.
-    #[ignore = "Not implemented"]
     fn test_task() {
         let input = include_str!("../example.txt");
         let result = task(input);
         assert!(result.is_some());
-        assert_eq!(result.unwrap(), "");
+        assert_eq!(result.unwrap(), "43");
     }
 }

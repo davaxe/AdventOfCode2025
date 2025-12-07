48d3be21ec1d797c2d3d0e93a294b24b3bdb099d
David Axelsson <davidaxelsson5@gmail.com>
2025-12-07 13:47:30 +0100

    Day 07 complete

--- day-07/src/part1.rs
+++ day-07/src/part1.rs
@@ -1,6 +1,78 @@
+use std::collections::{HashMap, HashSet};
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Cell {
+    Start,
+    Empty,
+    Splitter,
+}
+
+impl TryFrom<char> for Cell {
+    type Error = ();
+
+    fn try_from(value: char) -> Result<Self, Self::Error> {
+        match value {
+            'S' => Ok(Self::Start),
+            '.' => Ok(Self::Empty),
+            '^' => Ok(Self::Splitter),
+            _ => Err(()),
+        }
+    }
+}
+
 #[must_use]
-pub fn task(_input: &str) -> Option<String> {
-    None
+pub fn parse_map(input: &str) -> HashMap<(i32, i32), Cell> {
+    input
+        .lines()
+        .enumerate()
+        .flat_map(|(y, line)| {
+            line.char_indices().filter_map(move |(x, c)| {
+                Some((
+                    (i32::try_from(x).ok()?, i32::try_from(y).ok()?),
+                    Cell::try_from(c).ok()?,
+                ))
+            })
+        })
+        .collect()
+}
+
+#[must_use]
+pub fn task(input: &str) -> Option<String> {
+    let map = parse_map(input);
+    let start = map
+        .iter()
+        .find_map(|(&pos, &c)| if c == Cell::Start { Some(pos) } else { None })?;
+
+    let mut beams = vec![start];
+    let mut total_splits = 0;
+    while !beams.is_empty() {
+        let (splits, new_beams) = step(&beams, &map);
+        total_splits += splits;
+        beams = new_beams;
+    }
+
+    Some(total_splits.to_string())
+}
+
+fn step(beams: &Vec<(i32, i32)>, map: &HashMap<(i32, i32), Cell>) -> (u32, Vec<(i32, i32)>) {
+    let mut new_beams = HashSet::with_capacity(beams.len() * 2);
+    let mut splits = 0;
+    for &(x, y) in beams {
+        let (nx, ny) = (x, y + 1);
+        match map.get(&(nx, ny)) {
+            Some(Cell::Empty | Cell::Start) => {
+                new_beams.insert((nx, ny));
+            }
+            Some(Cell::Splitter) => {
+                splits += 1;
+                new_beams.insert((nx + 1, ny));
+                new_beams.insert((nx - 1, ny));
+            }
+            None => {}
+        }
+    }
+
+    (splits, new_beams.into_iter().collect())
 }
 
 #[cfg(test)]
--- day-07/src/part2.rs
+++ day-07/src/part2.rs
@@ -1,6 +1,49 @@
+use crate::part1;
+use std::collections::HashMap;
+
 #[must_use]
-pub fn task(_input: &str) -> Option<String> {
-    None
+pub fn task(input: &str) -> Option<String> {
+    let map = part1::parse_map(input);
+    let start = map.iter().find_map(|(&pos, &c)| {
+        if c == part1::Cell::Start {
+            Some(pos)
+        } else {
+            None
+        }
+    })?;
+
+    let mut particles = HashMap::new();
+    let mut count: u64 = 0;
+    particles.insert(start, 1);
+    while !particles.is_empty() {
+        particles = step(particles, &map);
+        let count_sum = particles.values().sum();
+        count = count.max(count_sum);
+    }
+    Some(count.to_string())
+}
+
+fn step(
+    particles: HashMap<(i32, i32), u64>,
+    map: &HashMap<(i32, i32), part1::Cell>,
+) -> HashMap<(i32, i32), u64> {
+    let mut new = HashMap::new();
+
+    for ((x, y), count) in particles {
+        let (nx, ny) = (x, y + 1);
+        match map.get(&(nx, ny)) {
+            Some(part1::Cell::Empty | part1::Cell::Start) => {
+                *new.entry((nx, ny)).or_insert(0) += count;
+            }
+            Some(part1::Cell::Splitter) => {
+                *new.entry((nx + 1, ny)).or_insert(0) += count;
+                *new.entry((nx - 1, ny)).or_insert(0) += count;
+            }
+            None => {}
+        }
+    }
+
+    new
 }
 
 #[cfg(test)]
@@ -8,12 +51,10 @@ mod tests {
     use super::*;
 
     #[test]
-    // TODO: Implement test, and remove ignore.
-    #[ignore = "Not implemented"]
     fn test_task() {
         let input = include_str!("../example.txt");
         let result = task(input);
         assert!(result.is_some());
-        assert_eq!(result.unwrap(), "");
+        assert_eq!(result.unwrap(), "40");
     }
 }

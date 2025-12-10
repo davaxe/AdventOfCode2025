a4ea2def14b51da7eebea37a79f18daaa1c4cc14
David Axelsson <david.axelsson@liu.se>
2025-12-10 15:19:36 +0100

    Part two complete (cheat)

--- day-10/src/part2.rs
+++ day-10/src/part2.rs
@@ -1,19 +1,38 @@
 use crate::{Machine, NoLightsMachine, parse};
 use chumsky::prelude::*;
+use microlp::{ComparisonOp, LinearExpr, OptimizationDirection, Problem};
 
 #[must_use]
 pub fn task(input: &str) -> Option<String> {
     let machines = parse::machines().parse(input).into_output()?;
-    let res: u32 = machines
+    let res: f64 = machines
         .into_iter()
         .map(Machine::to_no_lights_machine)
-        .filter_map(solve_machine)
+        .filter_map(|machine| solve_machine(&machine))
         .sum();
     Some(res.to_string())
 }
 
-fn solve_machine(machine: NoLightsMachine) -> Option<u32> {
-    todo!()
+fn solve_machine(machine: &NoLightsMachine) -> Option<f64> {
+    let x_n = machine.buttons.len();
+    let mut problem = Problem::new(OptimizationDirection::Minimize);
+    let mut vars = vec![];
+    for _ in 0..x_n {
+        vars.push(problem.add_integer_var(1.0, (0, i32::MAX)));
+    }
+
+    for (r, &rhs) in machine.requirements.iter().enumerate() {
+        let mut expr = LinearExpr::empty();
+        for (c, var) in vars.iter().enumerate() {
+            if !machine.buttons[c].connections.contains(&r) {
+                continue;
+            }
+            expr.add(*var, 1.0);
+        }
+        problem.add_constraint(expr, ComparisonOp::Eq, f64::from(rhs));
+    }
+    let solution = problem.solve().ok()?;
+    Some(solution.objective().round())
 }
 #[cfg(test)]
 mod tests {

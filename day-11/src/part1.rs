d9c409df9be6a866f15ca0906c8692964a9dd3ed
David Axelsson <david.axelsson@liu.se>
2025-12-11 11:29:02 +0100

    Part one complete

--- day-11/src/part1.rs
+++ day-11/src/part1.rs
@@ -1,6 +1,60 @@
+use std::collections::HashMap;
+
+#[derive(Debug)]
+pub struct Device<'a> {
+    pub name: &'a str,
+    pub connections: Vec<&'a str>,
+}
+
+impl Device<'_> {
+    pub fn new<'a>(name: &'a str, connections: Vec<&'a str>) -> Device<'a> {
+        Device { name, connections }
+    }
+}
+
 #[must_use]
-pub fn task(_input: &str) -> Option<String> {
-    None
+pub fn task(input: &str) -> Option<String> {
+    let mut devices: HashMap<&str, Device<'_>> = input
+        .lines()
+        .filter_map(|line| {
+            let (name, connections) = line.split_once(':')?;
+
+            Some((
+                name.trim(),
+                Device::new(
+                    name.trim(),
+                    connections.split_whitespace().map(str::trim).collect(),
+                ),
+            ))
+        })
+        .collect();
+
+    // "out" does not appear as a device in the input, so we add it manually
+    devices.insert("out", Device::new("out", Vec::new()));
+    let idx_map: HashMap<&str, usize> = devices
+        .keys()
+        .enumerate()
+        .map(|(idx, &name)| (name, idx))
+        .collect();
+
+    let mut queue = vec![("you", vec![false; devices.len()])];
+    let mut paths = 0;
+    while let Some((current, mut visited)) = queue.pop() {
+        if current == "out" {
+            paths += 1;
+            continue;
+        }
+        visited[idx_map[current]] = true;
+
+        if let Some(device) = devices.get(current) {
+            for &conn in &device.connections {
+                if !visited[idx_map[conn]] {
+                    queue.push((conn, visited.clone()));
+                }
+            }
+        }
+    }
+    Some(paths.to_string())
 }
 
 #[cfg(test)]
@@ -8,12 +62,10 @@ mod tests {
     use super::*;
 
     #[test]
-    // TODO: Implement test, and remove ignore.
-    #[ignore = "Not implemented"]
     fn test_task() {
         let input = include_str!("../example.txt");
         let result = task(input);
         assert!(result.is_some());
-        assert_eq!(result.unwrap(), "");
+        assert_eq!(result.unwrap(), "5");
     }
 }

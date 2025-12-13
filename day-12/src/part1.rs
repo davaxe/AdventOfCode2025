55c72cbe04978a45d7c8f819d22ce3416140b29e
David Axelsson <davidaxelsson5@gmail.com>
2025-12-13 11:53:54 +0100

    Day 12 prep

---
+++ day-12/src/part1.rs
@@ -0,0 +1,17 @@
+#[must_use]
+pub fn task(_input: &str) -> Option<String> {
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_task() {
+        let input = include_str!("../example.txt");
+        let result = task(input);
+        assert!(result.is_some());
+        assert_eq!(result.unwrap(), "2");
+    }
+}
